92bc7b89ae705a50b71b61fe05389d9229607399
codeCadaver <mike@codecadaver.com>
2021-07-14 16:57:59 -0400

    working on jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _jumpForce = 5f;
+
+    private bool _isGrounded = false;
     private Rigidbody2D _rigidbody2D;
 
     private void Start()
@@ -14,15 +17,39 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-
-        _rigidbody2D.velocity = new Vector2(h, _rigidbody2D.velocity.y);
+        Movement();
+        // Debug.DrawRay(transform.position, Vector3.down * 0.6f, Color.green);
     }
 
     private void Movement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
+        float move = Input.GetAxisRaw("Horizontal");
+        
+        Jump();
+        
+        _rigidbody2D.velocity = new Vector2(move, _rigidbody2D.velocity.y);
+    }
 
-        _rigidbody2D.velocity = new Vector2(horizontal, _rigidbody2D.velocity.y);
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IsGrounded();
+            if (!_isGrounded) { return; }
+
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
+            _isGrounded = false;
+        }
+    }
+
+    private void IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << 8);
+        
+        if (hit.collider == null) return;
+        
+        _isGrounded = true;
+
+        Debug.Log($"Collided with: {hit.collider.name}");
     }
 }
