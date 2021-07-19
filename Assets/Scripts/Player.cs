925a955925dde52654af5f163e7cbe6c350182d4
codeCadaver <mike@codecadaver.com>
2021-07-19 09:14:23 -0400

    Attack FX animation added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,23 +8,27 @@ public class Player : MonoBehaviour
     public static Action OnPlayerAttacked;
     public static Action<float> OnPlayerMoved;
     public static Action<bool> OnPlayerJumping;
-    
+
+    [SerializeField] private float _attackDelay = 0.2f;
     [SerializeField] private float _groundCheckDistance = 0.7f;
     [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private float _speed = 5f;
 
+    private bool _canAttack = true;
     private bool _canMove = true;
     private bool _isGrounded = false;
     private bool _resetJump = true;
-    // private PlayerAnimation _playerAnimation;
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _playerSprite;
+    private SpriteRenderer _swordArcSprite;
+    private Vector3 _localArcPos;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        // _playerAnimation = GetComponent<PlayerAnimation>();
         _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        _swordArcSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        _localArcPos = _swordArcSprite.transform.localPosition;
     }
 
     private void Update()
@@ -58,12 +62,25 @@ public class Player : MonoBehaviour
         {
             if (IsGrounded())
             {
-                _canMove = false;
-                _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
-                OnPlayerAttacked?.Invoke();
+                if (_canAttack)
+                {
+                    StartCoroutine(AttackRoutine());
+                }
+                // _canMove = false;
+                // _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
+                // OnPlayerAttacked?.Invoke();
                 // _playerAnimation.Attack();
             }
         }
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
+        OnPlayerAttacked?.Invoke();
+        _canAttack = false;
+        yield return new WaitForSeconds(_attackDelay);
+        _canAttack = true;
     }
 
     public void CanMove()
@@ -107,7 +124,17 @@ public class Player : MonoBehaviour
     {
         if (speed != 0)
         {
+            Vector3 xPos = _localArcPos;
             _playerSprite.flipX = speed < 0 ? true : false;
+            _swordArcSprite.flipY = speed < 0 ? true : false;
+            xPos.x *= Mathf.Sign(speed) * _localArcPos.x;
+            
+            //TODO: adjust rotation of sword arc 
+            if (Mathf.Sign(speed) < 0)
+            {
+                xPos.x -= 0.3f;
+            }
+            _swordArcSprite.transform.localPosition = xPos;
         }
     }
 
--- Assets/Scripts/SpriteHelper.cs
+++ Assets/Scripts/SpriteHelper.cs
@@ -1,16 +1,71 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+
+public enum SwordFX
+{
+    NONE = 0,
+    BLUE = 3,
+    RED = 2,
+    ORANGE = 1
+};
 
 public class SpriteHelper : MonoBehaviour
 {
     public static Action OnAttackEnded;
+    public static Action OnAttackBegan;
+
+    
+    [SerializeField] private GameObject[] _swordFX;
+    private GameObject _currentSpriteFX;
+
+    public SwordFX _currentFX;
+
+    private void Start()
+    {
+        _currentFX = SwordFX.ORANGE;
+        _currentSpriteFX = _swordFX[0];
+    }
+
+    private void Update()
+    {
+        switch (_currentFX)
+        {
+            case SwordFX.NONE:
+                _currentSpriteFX = null;
+                break;
+            case SwordFX.BLUE:
+                _currentSpriteFX = _swordFX[3];
+                break;
+            case SwordFX.ORANGE:
+                _currentSpriteFX = _swordFX[0];
+                break;
+            case SwordFX.RED:
+                _currentSpriteFX = _swordFX[1];
+                break;
+            default:
+                _currentSpriteFX = null;
+                break;
+        }
+    }
+
 
     public void AttackEnded()
     {
         OnAttackEnded?.Invoke();
+        foreach (var fx in _swordFX)
+        {
+            fx.SetActive(false);
+        }
     }
 
-
+    public void AttackBegan()
+    {
+        OnAttackBegan?.Invoke();
+        // _currentSpriteFX.SetActive(true);
+        // _currentSpriteFX.GetComponent<Animator>().SetTrigger("Attack");
+    }
+    
 }
