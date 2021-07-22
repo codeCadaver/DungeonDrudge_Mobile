737efd7851d49f98e54490ec47ebb0432943aba9
codeCadaver <mike@codecadaver.com>
2021-07-22 11:55:10 -0400

    spider enemy added

--- Assets/Scripts/MossGiant.cs
+++ Assets/Scripts/MossGiant.cs
@@ -6,30 +6,58 @@ using UnityEngine.EventSystems;
 
 public class MossGiant : Enemy
 {
+    private Animator _animator;
     private bool _movingRight = true;
+    private int _idleHash = Animator.StringToHash("Idle");
+    private SpriteRenderer _sprite;
     
     private void Start()
     {
-        
+        _animator = GetComponentInChildren<Animator>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
     } 
 
     public override void Update()
     {
-        // if at start, moving right = true
-        // if at end, moving right = false
-        // move right at beginning
         Movement(_movingRight);
     }
 
     private void Movement(bool direction)
     {
+        // don't move character while in idle
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("MossGiant_Idle"))
+        {
+            return;
+        }
+        
+        // face forward direction
+        _sprite.flipX = !_movingRight;
+        
         Transform target = direction ? end : start;
         
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-            
+
         if (transform.position == end.position)
+        {
             _movingRight = false;
-        if (transform.position == start.position)
+        }
+
+        else if (transform.position == start.position)
+        {
             _movingRight = true;
+        }
+
+        if (transform.position == target.position)
+        {
+            _animator.SetTrigger(_idleHash);
+        }
+
+        
+    }
+
+    IEnumerator FlipDelayRoutine(bool flip)
+    {
+        yield return new WaitForSeconds(0.5f);
+        _sprite.flipX = flip;
     }
 }
--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -5,13 +5,47 @@ using UnityEngine.Timeline;
 
 public class Spider : Enemy
 {
+    private Animator _animator;
+    private bool _movingRight = true;
+    private int _idleHash = Animator.StringToHash("Idle");
+    private SpriteRenderer _sprite;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponentInChildren<Animator>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Update()
     {
+        Movement(_movingRight);
+    }
+
+    private void Movement(bool moveRight)
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Spider_Idle"))
+        {
+            return;
+        }
+        _sprite.flipX = !moveRight;
         
+        Transform target = moveRight ? end : start;
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        if (transform.position == target.position)
+        {
+            _animator.SetTrigger(_idleHash);
+        }
+
+        if (transform.position == start.position)
+        {
+            _movingRight = true;
+        }
+        else if (transform.position == end.position)
+        {
+            _movingRight = false;
+        }
     }
 }
