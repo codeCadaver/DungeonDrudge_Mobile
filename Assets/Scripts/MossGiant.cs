969b308f5f5471728e404f5f2ef2525d79e4d3bb
codeCadaver <mike@codecadaver.com>
2021-07-23 17:12:00 -0400

    Moss Giant has attack system

--- Assets/Scripts/MossGiant.cs
+++ Assets/Scripts/MossGiant.cs
@@ -7,7 +7,13 @@ using UnityEngine.EventSystems;
 public class MossGiant : Enemy, IDamageable
 {
     public int Health { get; set; }
-    
+
+    public override void Init()
+    {
+        base.Init();
+        Health = base.health;
+    }
+
     protected override void Start()
     {
         base.Start();
@@ -21,10 +27,23 @@ public class MossGiant : Enemy, IDamageable
     public override void Movement(bool direction)
     {
         base.Movement(direction);
+        if (animator.GetBool(combatHash) == true)
+        {
+            Vector3 playerDirection = player.transform.localPosition - transform.localPosition;
+            sprite.flipX = playerDirection.x < 0 ? true : false;
+        }
     }
 
     public void Damage()
     {
-        
+        canMove = false;
+        Health -= 1;
+        animator.SetTrigger(hitHash);
+        animator.SetBool(combatHash, true);
+        if (Health <= 0)
+        {
+            animator.SetBool(combatHash, false);
+            animator.SetTrigger("Dead");
+        }
     }
 }
